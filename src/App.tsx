bf1c546d3d7ebcd896fbd1823ead1700b8ac186e
hellomac87 <hellomac87@gmail.com>
2021-05-25 17:22:56 +0900

    [add] cart page - create cart page - add /cart router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from "page/HomePage";
 import ProductsPage from "page/ProductsPage";
+import CartPage from "page/CartPage";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/products" component={ProductsPage} />
+        <Route exact path="/cart" component={CartPage} />
       </Switch>
     </Router>
   );
