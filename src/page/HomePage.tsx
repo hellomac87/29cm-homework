1065c1b869b637ea9088bf26a95c8eea4cf0e082
hellomac87 <hellomac87@gmail.com>
2021-05-25 17:20:54 +0900

    [add] home - add markup && styl;e && link

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -1,5 +1,54 @@
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
 function HomePage() {
-  return <div>29cm homework</div>;
+  return (
+    <Container>
+      <h1>
+        {"29cm "}
+        {"FE-Homework"}
+      </h1>
+
+      <Links>
+        <NavLink to={"/products"}>{"products"}</NavLink>
+        <NavLink to={"/cart"}>{"cart"}</NavLink>
+      </Links>
+    </Container>
+  );
 }
 
 export default HomePage;
+
+const Container = styled.main`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+
+  h1 {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+
+    padding: 36px 86px;
+
+    background-color: #333;
+    color: #fff;
+    font-size: 48px;
+
+    margin-bottom: 24px;
+  }
+`;
+
+const Links = styled.div`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  a {
+    font-size: 24px;
+    padding: 0 12px;
+  }
+`;
