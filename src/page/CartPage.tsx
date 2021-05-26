6ce4608dc1564b1699185cde241c95be362403ec
hellomac87 <hellomac87@gmail.com>
2021-05-26 21:29:48 +0900

    [fix] cart page - divide filter func - divide & fix calcTotal func

--- src/page/CartPage.tsx
+++ src/page/CartPage.tsx
@@ -6,6 +6,7 @@ import styled from "styled-components";
 import Error from "components/Error";
 import Loader from "components/Loader";
 import useLocalStorage from "hooks/useLocalStorage";
+import { CartItem } from "store/types/cart";
 
 function CartPage() {
   const dispatch = useDispatch();
@@ -21,6 +22,7 @@ function CartPage() {
   function increaseAmout(item_no: number) {
     dispatch(cartSlice.actions.increaseAmountByItemNo(item_no));
   }
+
   function decreaseAmout(item_no: number) {
     dispatch(cartSlice.actions.decreaseAmountByItemNo(item_no));
   }
@@ -29,13 +31,19 @@ function CartPage() {
     return price * amount;
   }
 
-  const calcTotalPrice = useCallback(() => {
-    const total = data.reduce((acc, next) => {
+  function filterByCartIds(cartItems: CartItem[]) {
+    return cartItems.filter((cartItem) =>
+      cartItemIds.includes(cartItem.item_no)
+    );
+  }
+
+  function calcTotalPrice(cartItems: CartItem[]) {
+    const total = cartItems.reduce((acc, next) => {
       return acc + next.price * next.amount;
     }, 0);
 
     return total;
-  }, [data]);
+  }
 
   useEffect(() => {
     dispatch(fetchCart());
@@ -44,47 +52,50 @@ function CartPage() {
   if (fetching || !data) return <Loader />;
   if (error) return <Error />;
 
+  const cartItems = filterByCartIds(data);
+
   return (
     <Container>
       <h1>{"CartPage"}</h1>
       <Table>
-        {data
-          .filter((product) => cartItemIds.includes(product.item_no))
-          .map((product) => {
-            return (
-              <Row key={product.item_no}>
-                <ColCheckBox>
-                  <input type="checkbox" checked={false} onChange={() => {}} />
-                </ColCheckBox>
-                <ColImage>
-                  <img src={product.detail_image_url} alt={product.item_name} />
-                </ColImage>
-                <ColName>{product.item_name}</ColName>
-                <ColAmount>
-                  <AmountButton
-                    type="button"
-                    onClick={() => decreaseAmout(product.item_no)}
-                  >
-                    {"-"}
-                  </AmountButton>
-                  <Amount>{product.amount}</Amount>
-                  <AmountButton
-                    type="button"
-                    onClick={() => increaseAmout(product.item_no)}
-                  >
-                    {"+"}
-                  </AmountButton>
-                </ColAmount>
-                <ColPrice>
-                  {calcPriceByAmount(product.price, product.amount)}
-                </ColPrice>
-              </Row>
-            );
-          })}
+        {cartItems.map((product) => {
+          return (
+            <Row key={product.item_no}>
+              <ColCheckBox>
+                <input type="checkbox" checked={false} onChange={() => {}} />
+              </ColCheckBox>
+              <ColImage>
+                <img src={product.detail_image_url} alt={product.item_name} />
+              </ColImage>
+              <ColName>
+                {product.item_name}
+                <Price>{product.price}</Price>
+              </ColName>
+              <ColAmount>
+                <AmountButton
+                  type="button"
+                  onClick={() => decreaseAmout(product.item_no)}
+                >
+                  {"-"}
+                </AmountButton>
+                <Amount>{product.amount}</Amount>
+                <AmountButton
+                  type="button"
+                  onClick={() => increaseAmout(product.item_no)}
+                >
+                  {"+"}
+                </AmountButton>
+              </ColAmount>
+              <ColPrice>
+                {calcPriceByAmount(product.price, product.amount)}
+              </ColPrice>
+            </Row>
+          );
+        })}
 
         <TotalRow>
           {"합계 금액 "}
-          {calcTotalPrice()}
+          {calcTotalPrice(cartItems)}
         </TotalRow>
       </Table>
     </Container>
@@ -130,6 +141,10 @@ const ColImage = styled.div`
 
 const ColName = styled.div`
   width: 35%;
+`;
+
+const Price = styled.div`
+  width: 100%;
 `;
 
 const ColAmount = styled.div`
